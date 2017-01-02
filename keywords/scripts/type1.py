b9641fabe61c5a3c9e00073773a62c53652c2ec5
Fearnley Martin <fearnleymartin@gmail.com>
2017-01-02 15:33:51 +0100

    initial keywords of type 1

---
+++ keywords/scripts/type1.py
@@ -0,0 +1,14 @@
+'''
+For generating type 1 keywords of the form: <degree_type> + <subject>
+'''
+
+# Add in more variations later
+degree_types = ['bachelor\'s', 'master\'s', 'phd']
+
+with open('../type1.txt', 'w') as of:
+    with open('../subjects2.txt') as ifile:
+        subjects = ifile.readlines()
+        for degree_type in degree_types:
+            for subject in subjects:
+                of.write(degree_type + ' ' + subject)
+
